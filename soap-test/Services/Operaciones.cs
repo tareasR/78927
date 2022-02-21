41ac9ebf6105f15f4487558a1f55e3da96c4359c
tareasr <tareasrojano@gmail.com>
2022-02-21 18:44:50 +0000

    soap-test en docker

---
+++ soap-test/Services/Operaciones.cs
@@ -0,0 +1,16 @@
+using System;
+using WSDL.mensajes;
+
+namespace WSDL.operaciones
+{
+ public class Operaciones:Mensajes
+ {
+  public string Saludar (string nombre){
+   string msj="Hola "+nombre+", te saluda Rafael";
+   return msj;
+  }
+  public string Mostrar (int id){
+   return "x";
+  }
+ }
+}
